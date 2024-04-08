4ff46008d00a9ba219a5828bf879e254eb7fc341
jysndabuio <jysndabu@outlook.com>
2024-04-08 23:52:35 +0200

    Removed DRY

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,18 @@
 pub fn number(user_number: &str) -> Option<String> {
     
     // Remove non digit character even whitespaces
-    let mut only_number = String::new();
-    for c in user_number.chars(){
-        if c.is_ascii_digit(){
-            only_number.push(c)
-        }
-    }
+    let only_number: String = user_number.chars().filter(|c| c.is_ascii_digit()).collect();
+    
 
     match only_number.len() {
         10 => {
-            if only_number.chars().nth(0) != Some('0') &&  only_number.chars().nth(0) != Some('1') && 
-                only_number.chars().nth(3) != Some('0') && only_number.chars().nth(3) != Some('1') {
+            // It workds but... the process is checking  the first 4 numbers if != 0 or 1. 
+            if only_number[0..=3].chars().all(|x| x != '0' && x != '1') {
                     return Some(only_number)
                 }   
         }
         11 => {
-            if only_number.chars().nth(0) == Some('1') && only_number.chars().nth(1) != Some('0') && 
-                only_number.chars().nth(1) != Some('1') && only_number.chars().nth(4) != Some('0') && 
-                only_number.chars().nth(4) != Some('1') {
+            if only_number.chars().nth(0) == Some('1') && only_number[1..=4].chars().all(|x| x != '0' && x != '1') {
                     let sms_number = only_number[1..].to_string();
                     return Some(sms_number)
                 }       
