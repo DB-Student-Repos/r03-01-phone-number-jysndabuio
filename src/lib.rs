e0459f5970879d218d8d9494394d3e13e1fe5737
jysndabuio <jysndabu@outlook.com>
2024-04-09 21:48:09 +0200

    Checking Ns

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,27 @@
 pub fn number(user_number: &str) -> Option<String> {
     
-    // Remove non digit character even whitespaces
     let only_number: String = user_number.chars().filter(|c| c.is_ascii_digit()).collect();
     
-
     match only_number.len() {
         10 => {
-            // It workds but... the process is checking  the first 4 numbers if != 0 or 1. 
-            if only_number[0..=3].chars().all(|x| x != '0' && x != '1') {
-                    return Some(only_number)
-                }   
+            if check_ns(&only_number, "0") && check_ns(&only_number, "3") {
+                return Some(only_number);
+            }   
         }
         11 => {
-            if only_number.chars().nth(0) == Some('1') && only_number[1..=4].chars().all(|x| x != '0' && x != '1') {
-                    let sms_number = only_number[1..].to_string();
-                    return Some(sms_number)
-                }       
+            if only_number.starts_with('1') && check_ns(&only_number, "1") && check_ns(&only_number, "4")  {
+                let sms_number = only_number[1..].to_string();
+                return Some(sms_number);
+            }       
         }
-        _ =>  {}
+        _ => {}
     }
-
     None
+}
+
+fn check_ns(only_number: &str, x: &str) -> bool {
+    if let Some(c) = only_number.chars().nth(x.parse().unwrap()) {
+        return c != '0' && c != '1';
+    }
+    false
 }
